5e55a16c40d7ba27ae592e496b613fa3698b6e44
RajeenaJabir <rajeena.m2018@gmail.com>
2021-12-16 22:28:31 +0530

    firefox driver launch in new package

--- src/main/java/com/obsqura/basic/Test.java
+++ src/main/java/com/obsqura/basic/Test.java
@@ -1,7 +1,47 @@
 package com.obsqura.basic;
 
-public class Test {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test{
+    static WebDriver driver;
+    public static void testInitl(String browser) {
+        if (browser.equals("Chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\SELENIUM\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser.equals("Edge")) {
+            System.setProperty("webdriver.edge.driver", "C:\\SELENIUM\\msedgedriver.exe");
+            driver = new EdgeDriver();
+        } else if(browser.equals("Firefox")){
+            System.setProperty("webdriver.gecko.driver", "C:\\SELENIUM\\geckodriver.exe");
+            driver = new FirefoxDriver();
+        }else{
+            try {
+                throw new Exception("Invalid browser exception");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+
+
     public static void main(String args[]) {
-        System.out.println("hai hello");
+
+        testInitl("Firefox");
+        driver.get("http://demo.guru99.com/test/newtours/");
+        String actualTitle=driver.getTitle();
+        //System.out.println(actualTitle);
+        String actualUrl=driver.getCurrentUrl();
+        //System.out.println(actualUrl);
+        String pageSource=driver.getPageSource();
+        System.out.println(pageSource);
+       // driver.close();
+
+
     }
 }
